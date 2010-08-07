0084af263c968f5243371ecb99e7426416afc7bd
Nigel Cunningham <nigel@tuxonice.net>
2010-08-07 13:51:48 +1000

    Remove unnecessary symbol exports.
    Remove the now-unneeded export of the device_pm_lock and
    device_pm_unlock functions.
    
    Signed-off-by: Nigel Cunningham <nigel@tuxonice.net>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -69,7 +69,6 @@ void device_pm_lock(void)
 {
 	mutex_lock(&dpm_list_mtx);
 }
-EXPORT_SYMBOL_GPL(device_pm_lock);
 
 /**
  * device_pm_unlock - Unlock the list of active devices used by the PM core.
@@ -78,7 +77,6 @@ void device_pm_unlock(void)
 {
 	mutex_unlock(&dpm_list_mtx);
 }
-EXPORT_SYMBOL_GPL(device_pm_unlock);
 
 /**
  * device_pm_add - Add a device to the PM core's list of active devices.
