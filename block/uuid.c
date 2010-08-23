9336992e83c13358b2d53e30bc0c4857ff434b52
Nigel Cunningham <nigel@tuxonice.net>
2010-08-23 11:12:29 +1000

    Correct format in uiid debugging code
    Change formats in uuid debugging code from %x to %lx.
    
    Signed-off-by: Nigel Cunningham <nigel@tuxonice.net>

--- block/uuid.c
+++ block/uuid.c
@@ -334,11 +334,11 @@ int part_matches_fs_info(struct hd_struct *part, struct fs_info *seek)
 				result = 3;
 				PRINTK(" Matching dev_t.\n");
 			} else
-				PRINTK("Dev_ts differ (%x vs %x).\n", part_devt(part), seek->dev_t);
+				PRINTK("Dev_ts differ (%lx vs %lx).\n", part_devt(part), seek->dev_t);
 		}
 	}
 
-	PRINTK(" Score for %x is %d.\n", part_devt(part), result);
+	PRINTK(" Score for %lx is %d.\n", part_devt(part), result);
 	free_fs_info(got);
 out:
 	blkdev_put(bdev, FMODE_READ);
