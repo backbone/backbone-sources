403d7c92e3cec6a07aee7f1554e234f151aaeecc
François Valenduc <francois.valenduc@tvcablenet.be>
2012-10-04 20:08:26 +0200

    TuxOnice: export __page_file_index
    This is needed by Tuxonice to compile nfs as a module.
    
    Signed-off-by: François Valenduc <francois.valenduc@tvcablenet.be>
    Cc: Tuxonice-devel <tuxonice-devel@tuxonice.net>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2304,6 +2304,8 @@ pgoff_t __page_file_index(struct page *page)
 	return swp_offset(swap);
 }
 
+EXPORT_SYMBOL_GPL(__page_file_index);
+
 struct swap_info_struct *get_swap_info_struct(unsigned type)
 {
 	return swap_info[type];
