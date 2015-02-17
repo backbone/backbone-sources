6d24e3b8c84b3491752c15589eacb4f64945923b
Oleksandr Natalenko <oleksandr@natalenko.name>
2015-02-17 20:15:02 +0200

    toi-3.18: attempt to fix compiling without TOI

--- kernel/power/snapshot.c
+++ kernel/power/snapshot.c
@@ -101,8 +101,10 @@ static void *get_image_page(gfp_t gfp_mask, int safe_needed)
 {
 	void *res;
 
+#ifdef CONFIG_TOI
         if (toi_running)
             return (void *) toi_get_nonconflicting_page();
+#endif
 
 	res = (void *)get_zeroed_page(gfp_mask);
 	if (safe_needed)
